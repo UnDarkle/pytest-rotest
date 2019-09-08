d4efe4a8c19abb48f897272a58893511e225696a
Greg <gregoil@walla.co.il>
2019-09-08 12:28:22 +0300

    added ut

--- tests/test_rotest.py
+++ tests/test_rotest.py
@@ -1,1 +1,30 @@
 # -*- coding: utf-8 -*-
+from rotest.core import TestCase, TestFlow, TestBlock, BlockInput, BlockOutput
+
+
+class RotestCase(TestCase):
+    """A passing test case."""
+    def test_method(self):
+        pass
+
+
+class RotestBlockOutput(TestBlock):
+    """A test block that creates an output."""
+    value = BlockOutput()
+
+    def test_method(self):
+        self.value = 5
+
+
+class RotestBlockInput(TestBlock):
+    """A test block that creates an output."""
+    value = BlockInput()
+
+    def test_method(self):
+        self.assertEqual(self.value, 5)
+
+
+class RotestFlow(TestFlow):
+    """An example for a test flow."""
+    blocks = (RotestBlockOutput,
+              RotestBlockInput)
